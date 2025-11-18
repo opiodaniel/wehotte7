be12c449f6ee33ab6ffbf614f1241ed9eb2639a5
Opio Daniel <danielopio540@gmail.com>
2025-11-18 17:45:52 +0300

    addition of JWT for security

--- wehotte7/settings.py
+++ wehotte7/settings.py
@@ -77,7 +77,10 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 
